9d4935542fdac646d7271a1a002b13488409a217
Dimitris <dimitris.giokas1@gmail.com>
2020-05-17 10:27:01 +0200

    added hello world

--- src/main/java/com/example/demo/NordicMotorhomeApplication.java
+++ src/main/java/com/example/demo/NordicMotorhomeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NordicMotorhomeApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello Spring world!");
         SpringApplication.run(NordicMotorhomeApplication.class, args);
     }
 
