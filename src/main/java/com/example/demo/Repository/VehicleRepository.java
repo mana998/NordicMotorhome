d4d929e1f99c5adf41fbfe60ddca89aab5977ede
Karolina Kwiatkowska <kblkwiatkowska@gmail.com>
2020-05-20 16:25:27 +0200

    Fix bug in addVehicle (Karolina)

--- src/main/java/com/example/demo/Repository/VehicleRepository.java
+++ src/main/java/com/example/demo/Repository/VehicleRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.Repository;
 
 import com.example.demo.Model.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -39,12 +40,38 @@ public class VehicleRepository { //Karolina
 
     //add a new vehicle to the database
     public Vehicle addVehicle(Vehicle vehicle){
-        String sql="INSERT INTO brand (brand_name) VALUES (?)";
-        template.update(sql, vehicle.getBrand());
-        sql="INSERT INTO model (model_name, brandID, beds, price) VALUES (?, (SELECT brandID FROM brand WHERE brand_name = ?), ?, ?)";
-        template.update(sql, vehicle.getModel(), vehicle.getBrand(), vehicle.getBeds(), vehicle.getPrice());
-        sql="INSERT INTO vehicle (plates, is_available, modelID, brandID) VALUES (?,?,(SELECT modelID FROM model WHERE model_name = ?),(SELECT brandID FROM brand WHERE brand_name = ?))";
-        template.update(sql, vehicle.getPlates(), vehicle.isIsAvailable(), vehicle.getModel(), vehicle.getBrand());
+        int modelId;
+        int brandId;
+        //check if the model already exist
+        String sql = "SELECT modelID FROM model WHERE model_name = ?";
+        try {
+            //if model exists, modelID gets assigned to modelId variable
+            modelId = template.queryForObject(sql, Integer.class, vehicle.getModel());
+            //assign brandID of selected model to brandId variable
+            brandId = template.queryForObject("SELECT brandID FROM model WHERE model_name = ?", Integer.class, vehicle.getModel());
+        } catch (IncorrectResultSizeDataAccessException e1){
+            //if model doesn't exist
+            //check if the brand exist already
+            sql = "SELECT brandID FROM brand WHERE brand_name = ?";
+            try {
+                //if brand exists, brandID gets assigned to brandId variable
+                brandId = template.queryForObject(sql, Integer.class, vehicle.getBrand());
+            } catch (IncorrectResultSizeDataAccessException e2){
+                //if brand doesn't exist, insert new brand to the database
+                sql = "INSERT INTO brand (brand_name) VALUES (?)";
+                template.update(sql, vehicle.getBrand());
+                //get id of the new brand
+                brandId = template.queryForObject("SELECT MAX(brandID) FROM brand", Integer.class);
+            }
+            //insert new model to the database
+            sql="INSERT INTO model (model_name, brandID, beds, price) VALUES (?, ?, ?, ?)";
+            template.update(sql, vehicle.getModel(), brandId, vehicle.getBeds(), vehicle.getPrice());
+            //get id of the new model
+            modelId = template.queryForObject("SELECT MAX(modelID) FROM model", Integer.class);
+        }
+        //insert new vehicle to the database
+        sql="INSERT INTO vehicle (plates, is_available, modelID, brandID) VALUES (?,?,?,?)";
+        template.update(sql, vehicle.getPlates(), vehicle.isIsAvailable(), modelId, brandId);
         return null;
     }
 
