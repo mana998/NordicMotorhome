f9ce17ee90d2ae2e23d156ae1ce4db85254617c1
mana998 <mana998@azet.sk>
2020-05-21 11:36:13 +0200

    Fix - rename getRenter to findRenterById (Marianna)

--- src/main/java/com/example/demo/Service/RenterService.java
+++ src/main/java/com/example/demo/Service/RenterService.java
@@ -19,6 +19,6 @@ public class RenterService { //Marianna
     public void addRenter(Renter renter) {renterRepository.addRenter(renter);}
 
 
-    public Renter findRenterById(int id) { return renterRepository.getRenter(id);}
+    public Renter findRenterById(int id) { return renterRepository.findRenterById(id);}
 
 }
