2f87a618430243568ccbec2736c435a24a377eb5
mana998 <mana998@azet.sk>
2020-06-02 14:11:18 +0200

    Code cleanup (Marianna)

--- src/main/java/com/example/demo/Repository/CountryRepository.java
+++ src/main/java/com/example/demo/Repository/CountryRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.Repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.SingleColumnRowMapper;
